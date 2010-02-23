1f0c751d88a83c78f75951952650656c294e9e31
stu <stu.axon@gmail.com>
2010-02-23 23:58:32 +0000

    Add help + usage to addpath.cmd

--- scripts/addpath.cmd
+++ scripts/addpath.cmd
@@ -6,12 +6,22 @@
 :: Call WSH script to add to path, then output
 :: new path
 
+if "%1"=="/?" goto usage
+if "%1"=="/l" goto add_to_local_path_option
+if "%1"=="/L" goto add_to_local_path_option
+
 cscript //nologo %~d0%~p0\addpath.js %*
 if ERRORLEVEL 1 goto already_in_path
 if ERRORLEVEL 2 goto does_not_exist
 
-if "%*"=="." PATH=%PATH%;%CD%
-if not "%*"=="." PATH=%PATH%;%*
+goto add_to_local_path
+
+:add_to_local_path_option
+shift
+
+:add_to_local_path
+if "%1"=="." PATH=%PATH%;%CD%
+if not "%1"=="." PATH=%PATH%;%1
 goto end
 
 :already_in_path
@@ -20,5 +30,17 @@ goto end
 
 :does_not_exist
 exit /B 1
+
+:usage
+echo Usage:
+echo   addpath path		Add path to the system path
+echo   addpath /l path	Add path to this prompts path
+echo.
+echo Example:
+echo   Add current path to registry:
+echo     addpath .
+echo.
+echo   This works as paths are converted to absolute paths.
+echo.
 
 :end
