3799f04a554c91be198cc429092003ad7ce48a50
stu <stu.axon@gmail.com>
2010-02-23 23:55:15 +0000

    setup.cmd can install needed modules

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -42,22 +42,48 @@ call scripts\addpath dev > NUL
 call scripts\addpath launchers > NUL
 call scripts\addpath scripts > NUL
 
-SET PRIMARY_PYTHON=NOT_FOUND
-for /F "tokens=1* delims= " %%a in ('"python --version 2>&1"') DO SET PRIMARY_PYTHON=%%b
+SET PYTHON_VERSION=NOT_FOUND
+for /F "tokens=1* delims= " %%a in ('"python --version 2>&1"') DO SET PYTHON_VERSION=%%b
 
-IF "%PRIMARY_PYTHON%"=="program or batch file." goto no_python
+IF "%PYTHON_VERSION%"=="program or batch file." goto no_python
 
 :has_python
 :: TODO - check correct python modules installed
 
-echo Make sure you have these python modules installed
-echo.
-echo pywin32   http://sourceforge.net/projects/pywin32/
-echo winshell  http://timgolden.me.uk/python/winshell.html
-echo.
 
+::   Find python
+
+set PYTHON_PATH=
+:: Get path to primary python
+for %%P in (%PATH%) do if exist %%P\python.exe if "%PYTHON_PATH%"=="" set PYTHON_PATH=%%P\
+if not "%PYTHON_PATH%"=="" goto has_python__install_modules
+
+:: Check for python 2.6
+set PYTHON_VERSION=2.6
+
+:: search registry
+set QUERY="HKLM\SOFTWARE\Python\PythonCore\%PYTHON_VERSION%\InstallPath"
+
+for /F "tokens=3* skip=4 delims=	 " %%a in ('reg.exe query "%QUERY%" /ve') DO (
+    if exist "%%b\python.exe" (
+        SET PYTHON_PATH=%%b
+    )
+)
+
+if "%PYTHON_PATH%"=="" goto no_python
+
+SET /P CHOICE=Install pywin32 and winshell [Y/n]
+
+if "%CHOICE%"=="N" goto end
+if "%CHOICE%"=="n" goto end
+
+:has_python__install_modules
+
+%PYTHON_PATH%\scripts\easy_install pywin32
+%PYTHON_PATH%\scripts\easy_install winshell
 
 goto python_end
+
 
 :no_python
 
@@ -67,6 +93,14 @@ echo Python was not found in the path, these commands will not be available:
 :dir /b scripts\*.py
 
 for %%p in (scripts\*.py) do echo %%~np
+
+
+echo To Fix this:  Install Python and these modules:
+echo.
+echo pywin32   http://sourceforge.net/projects/pywin32/
+echo winshell  http://timgolden.me.uk/python/winshell.html
+echo.
+
 
 
 goto python_end
